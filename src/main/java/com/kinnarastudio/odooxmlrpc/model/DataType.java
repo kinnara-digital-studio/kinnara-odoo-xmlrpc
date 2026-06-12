a049fed97fa0eefebee860094460ad6266efda9d
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2026-06-12 14:39:54 +0700

    add data type

--- src/main/java/com/kinnarastudio/odooxmlrpc/model/DataType.java
+++ src/main/java/com/kinnarastudio/odooxmlrpc/model/DataType.java
@@ -1,16 +1,28 @@
 package com.kinnarastudio.odooxmlrpc.model;
 
+/**
+ * Odoo field data types
+ */
 public enum DataType {
     STRING,
-    INTEGER;
+    INTEGER,
+    FLOAT,
+    BOOLEAN,
+    MANY2ONE,
+    MANY2MANY;
 
-    public static DataType parse(String string) {
-        switch (string.toLowerCase()) {
-            case "integer":
-            case "many2one":
-                return DataType.INTEGER;
-            default:
-                return DataType.STRING;
+    /**
+     * Get {@link DataType} from string value
+     * @param value string value
+     * @return {@link DataType}, default {@link #STRING}
+     */
+    public static DataType parse(String value) {
+        for (DataType dataType : DataType.values()) {
+            if (dataType.name().equalsIgnoreCase(value)) {
+                return dataType;
+            }
         }
+
+        return STRING;
     }
 }
