08a83bb3deb9416ed849b6c9bcb487a7a85acac6
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2026-07-02 14:34:58 +0700

    add value parser in DataType

--- src/main/java/com/kinnarastudio/odooxmlrpc/model/DataType.java
+++ src/main/java/com/kinnarastudio/odooxmlrpc/model/DataType.java
@@ -1,5 +1,12 @@
 package com.kinnarastudio.odooxmlrpc.model;
 
+import com.kinnarastudio.commons.Try;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 /**
  * Odoo field data types
  */
@@ -24,5 +31,41 @@ public enum DataType {
         }
 
         return STRING;
+    }
+
+    /**
+     * Parse value based on DataType
+     * @param rawValue
+     * @return
+     * @param <T>
+     * @throws NumberFormatException
+     */
+    public <T> T valueParser(Object rawValue) throws NumberFormatException {
+        switch (this) {
+            case STRING:
+                return (T) String.valueOf(rawValue);
+            case MANY2ONE:
+            case INTEGER:
+                return (T) Integer.valueOf(rawValue.toString());
+            case FLOAT:
+                return (T) Float.valueOf(rawValue.toString());
+            case BOOLEAN:
+                return (T) Boolean.valueOf(rawValue.toString().toLowerCase());
+            case MANY2MANY:
+                return (T) Optional.of(rawValue)
+                        .map(String::valueOf)
+                        .filter(Predicate.not(String::isEmpty))
+                        .map(s -> s.split(";"))
+                        .stream()
+                        .flatMap(Arrays::stream)
+                        .map(String::trim)
+                        .filter(Predicate.not(String::isEmpty))
+                        .map(Try.onFunction(Integer::valueOf, (NumberFormatException e) -> null))
+                        .filter(Objects::nonNull)
+                        .toArray(Integer[]::new);
+
+            default:
+                return (T) rawValue.toString();
+        }
     }
 }
